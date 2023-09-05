6d6b889f3f769b5ae08f54a0f85a7e7c641c636a
Progress C. Ezeamaka <ezeamakachisom2002@gmail.com>
2023-09-05 21:11:40 +0100

    create components

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import ProjectData from '../projects.json';
 
 const Project = () => {
   return (
-    <div>Project</div>
+    <div>
+      <h1>Projects</h1>
+      <ul>
+        {ProjectData.Projects.map((project, index) => (
+          <li key={index}>
+            <h2>{project.title}</h2>
+            <p>{project.description}</p>
+            <img src={project.img}/>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
