eada79d78f330654c80bd12595ff80e82859c44c
Progress C. Ezeamaka <ezeamakachisom2002@gmail.com>
2023-09-05 21:55:28 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Project from './pages/Project';
+import ContactMe from './pages/ContactMe';
+import DesktopNav from './componets/header/DesktopNav';
+import MobileNav from './componets/header/MobileNav';
 import './App.css';
 
 function App() {
   return (
     <>
+      <DesktopNav />
+      <MobileNav/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About/>} />
         <Route path="/project" element={<Project/>} />
+        <Route path="/contactme" element={<ContactMe/>} />
       </Routes>
     </>
   );
