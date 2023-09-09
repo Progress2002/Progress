b7cd484cb3b4f11ef72d2c2c34dc742b9d58a717
Progress C. Ezeamaka <ezeamakachisom2002@gmail.com>
2023-09-09 02:58:22 +0100

    Style ResumeBtn

--- src/componets/ResumeBtn.js
+++ src/componets/ResumeBtn.js
@@ -4,10 +4,12 @@ import { Link } from "react-router-dom";
 
 const ResumeBtn = () => {
   return (
-    <button className=" text-white text-lg text-center bg-opacity-60 duration-300 hover:bg-sc-color py-2 px-5 rounded bg-gray w-fit ">
+    <button className="bg-animate text-white text-lg text-center bg-opacity-60
+      duration-300 py-2 px-5 rounded bg-gray w-fit   hover:bg-animate-hover transition-all">
       <Link
         to="https://media.publit.io/file/Progress-Ezeamaka-Resume.html"
-        target="blank" className="flex gap-2 text-center text-white ">
+        target="blank"
+        className="flex gap-2 text-center text-white ">
         {" "}
         <span className="text-white">Resume</span>
         <ImDownload className="relative top-1" />
