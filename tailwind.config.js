3767f231fb702c06084e689aec119937e89baf67
Progress C. Ezeamaka <ezeamakachisom2002@gmail.com>
2023-09-09 02:55:32 +0100

    Add custom styles to tailwind.config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,11 +11,19 @@ module.exports = {
       }
     },
     colors: {
-      'pr-color': 'hsla(0,0%,83%,.6)',
-      'sc-color': '#ff6b00',
-      'white': '#fff',
-      'gray': 'gray',
-      'black': '#000'
+      pr_color: 'hsla(0,0%,83%,.6)',
+      sc_color: '#ff6b00',
+      gray: 'gray',
+      black: '#000',
+      body: "hsl(217, 18%, 14%)",
+      white: "hsl(0, 0%, 100%)",
+      text: "hsl(0, 0%, 83%)",
+      text_Light: "hsla(0, 0%, 83%, 0.6)",
+      buttonBg: "hsl(217, 10%, 25%)",
+      cyan: "hsl(166, 100%, 34%)",
+      cyan_dark: "hsla(166, 100%, 34%, 0.4)",
+      veryDark: "hsl(0, 0%, 0%)",
+      gray: "hsl(217, 13%, 27%)",
     }
   },
   plugins: [],
