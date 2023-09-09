8b3414f8caae5714940192c87c68283c7a12fd6d
Progress C. Ezeamaka <ezeamakachisom2002@gmail.com>
2023-09-09 02:56:38 +0100

    Create mobile navigation

--- src/componets/header/MobileNav.js
+++ src/componets/header/MobileNav.js
@@ -1,19 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import NavItem from "./NavItem";
 import ResumeBtn from "../ResumeBtn";
 import { CgClose } from "react-icons/cg";
 
 const MobileNav = ({ isActiveRoute, isActive, setIsActive }) => {
+  useEffect(() => {
+    document.body.style.overflowY = isActive ? "hidden" : "auto";
+  }, [isActive]);
   return (
     <div
-      className="mobile-nav md:hidden absolute h-screen top-0 bottom-0 bg-black right-0 w-3/4 flex justify-center items-center"
+      className="mobile-nav md:hidden fixed h-screen top-0 bottom-0
+       bg-black right-0 w-3/4 flex justify-center items-center duration-700 ease"
       style={{ right: isActive ? "0%" : "-100%" }}>
       <button
-        className="absolute top-7 right-7 text-4xl text-white duration-300 hover:text-gray"
+        className="absolute top-7 right-1 text-4xl text-white duration-300
+         hover:text-gray active:bg-gray  px-2 py-2 rounded-full active:bg-opacity-20"
         onClick={() => setIsActive(!isActive)}>
         <CgClose />
       </button>
-      <ul className="w-96 flex flex-col justify-center items-center gap-7">
+      <ul
+        className="w-96 flex flex-col justify-center items-center gap-7"
+        role="menu">
         <NavItem
           isActiveRoute={isActiveRoute}
           path="/"
--- src/componets/header/NavItem.js
+++ src/componets/header/NavItem.js
@@ -4,9 +4,16 @@ import { Link } from "react-router-dom";
 const NavItem = ({ path, value, isActiveRoute, isActive, setIsActive }) => {
   return (
     <li
-      className="text-lg text-white capitalize duration-300 hover:text-sc-color font-normal"
-      style={{ color: isActiveRoute(path) ? "#ff6b00" : "inherit" }}
-      onClick={setIsActive ? () => setIsActive(!isActive) : ""}>
+      className="text-lg  capitalize duration-100 py-2 cursor-pointer  font-normal
+       relative  before:absolute before:bottom-0 before:w-full before:h-1
+       before:rounded-full before:hover:bg-sc_color"
+      role="menuitem"
+      id="nav-item"
+      style={{
+        color: isActiveRoute(path) && "#ff6b00",
+        fontWeight: isActiveRoute(path) && "bolder",
+      }}
+      onClick={() => setIsActive && setIsActive(!isActive)}>
       <Link to={path}>{value}</Link>
     </li>
   );
