54551f18e8d1ff1c6ab12a484b53b92c8cef7f8f
Progress C. Ezeamaka <ezeamakachisom2002@gmail.com>
2023-09-15 20:12:10 +0100

    Create Resume btn

---
+++ src/components/ResumeBtn.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { ImDownload } from "react-icons/im";
+import { Link } from "react-router-dom";
+
+const ResumeBtn = ({ text }) => {
+  return (
+    <button className="bg-animate text-white text-base text-center bg-opacity-60
+      duration-300 rounded bg-gray w-fit   hover:bg-animate-hover transition-all font-medium">
+      <Link
+        to="https://media.publit.io/file/Progress-Ezeamaka-Resume.html"
+        target="blank"
+        className="flex gap-2 md:gap-3 text-center text-text hover:text-white py-2 px-4">
+        {" "}
+        <span>{text}</span>
+        <ImDownload className="relative top-[0.28] text-2xl" />
+      </Link>
+    </button>
+  );
+};
+
+export default ResumeBtn;
