e6a6979ab5bbc085d605a8f5df8746e22cae2062
Progress C. Ezeamaka <ezeamakachisom2002@gmail.com>
2023-10-01 20:27:37 +0100

    Create Technology component

---
+++ src/components/Technology.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Fade } from "react-awesome-reveal";
+
+const Technology = ({ icon, text }) => {
+  return (
+    <li className="grid h-24 duration-200 rounded-md cursor-pointer md:h-32 lg:h-36 bg-buttonBg place-items-center hover:scale-105">
+      <Fade delay={200}>
+        <div>{icon}</div>
+      </Fade>
+      <Fade delay={300}>
+        <div className="text-base font-normal tracking-wide text-white capitalize md:text-xl lg:text-2xl">
+          {text}
+        </div>
+      </Fade>
+    </li>
+  );
+};
+
+export default Technology;
