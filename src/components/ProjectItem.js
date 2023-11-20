eb42448b6bdac538009bf11b819d0d5b010ae247
Progress C. Ezeamaka <ezeamakachisom2002@gmail.com>
2023-11-20 12:11:16 +0100

    Make Project page responsive

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -12,37 +12,35 @@ const ProjectItem = ({
   sourceCodeURL,
   technologies,
 }) => {
-  const [isFocus, setIsFocus] = useState(false);
-
-  console.log(isFocus);
   return (
     <li
       key={index}
-      className={`flex  ${
+      className={`lg:flex  ${
         index % 2 !== 0 && "flex-row-reverse"
-      } justify-between items-center hover:${isFocus && "cursor-pointer"}`}
-      onMouseOver={() => setIsFocus(true)}
-      onMouseOut={() => setIsFocus(false)}>
-      <div className="w-[70%] h-96 relative -z-[2] ">
-        <img src={img} className="w-full h-full hover:cursor-pointer" />
+      } justify-between items-center relative shadow-2xl lg:shadow-none`}>
+      {/* Image section of project item */}
+      <div className="w-full lg:w-[70%] h-[22rem] lg:h-96 rounded-lg relative -z-[2] ">
+        <img
+          src={img}
+          className="w-full h-full rounded-lg lg:rounded-[0] hover:cursor-pointer"
+        />
         <div
-          className={`h-full w-full absolute delay- bg-veryDark bg-opacity-[0.6] ${
-            isFocus && "hidden"
-          } hover:cursor-pointer z-[5] top-0`}></div>
+          className={` hidden lg:block h-full w-full absolute delay- bg-veryDark
+            bg-opacity-[0.6] rounded lg:rounded-[0] z-[5] top-0`}></div>
       </div>
-      <div className="w-[30%] ">
+      {/* description of project item  */}
+      <div
+        className={`w-full h-full lg:w-[30%] absolute bottom- top-0 opacity- lg:relative delay-200`}>
         <div
           className={` ${
-            index % 2 == 0 ? "-ml-60" : "-mr-60"
-          } bg-black bg-opacity-[0.7] delay-100 ${
-            isFocus && "bg-opacity-[0.9]"
-          } px-9 py-4 rounded-lg`}>
-          <h2 className="text-sc_color text-lg font-bold mb-3 text-center">
+            index % 2 == 0 ? "lg:-ml-60" : "lg:-mr-60"
+          } bg-black bg-opacity-[0.82] lg:bg-opacity-[0.7]  px-6 lg:px-9 py-4 rounded-lg h-full lg:shadow-lg`}>
+          <h2 className="text-sc_color text-xl lg:text-lg font-bold mb-3 text-center">
             {title}
           </h2>
-          <p className="md:leading-[1.5]">
+          <p className="md:leading-[1.5] text-[16px]">
             {description}
-            <div className="mt-5">
+            <div className="mt-5 mb-10">
               {technologies &&
                 technologies.map((item) => (
                   <span className="text-text_Light">{item} | </span>
@@ -51,8 +49,8 @@ const ProjectItem = ({
           </p>
         </div>
         <div
-          className={`flex relative top-14  ${
-            index % 2 == 0 && "justify-end"
+          className={`flex lg:relative lg:top-14 absolute bottom-2 w-full md:w-auto justify-between sm:justify-end lg:justify-between ${
+            index % 2 == 0 && "lg:justify-end"
           }`}>
           <Link
             to={liveURL}
--- src/pages/Project.js
+++ src/pages/Project.js
@@ -6,9 +6,10 @@ import { Slide, Fade } from "react-awesome-reveal";
 const Project = () => {
   const { Projects } = ProjectData;
   return (
-    <div className="min-h-full my-20 lg:container">
-      <div className=" text-start mx-7 md:mx-20 lg:mx-auto">
-        <h2 className="mb-16 text-4xl font-semibold text-center text-text md:text-5xl ">
+    <div className="min-h-full my-10 lg:my-20 lg:container">
+      <div className=" text-start mx-3 md:mx-20 lg:mx-auto">
+        <h2 className="mb-16 text-3xl sm:text-[3.5rem] lg:text-4xl
+          md:leading-[1.3] font-semibold text-center text-text md:text-5xl ">
           <span className="w-fit pb-2 mr-4">My Recent</span>
           <span
             className="text-sc_color relative  before:absolute
@@ -17,7 +18,7 @@ const Project = () => {
             Projects
           </span>
         </h2>
-        <ul className="flex flex-col gap-20">
+        <ul className="flex flex-col gap-14 lg:gap-20">
           {Projects.map((project, index) => (
             <Slide direction="up" triggerOnce="true" delay={100}>
               <ProjectItem
