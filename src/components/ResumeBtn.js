1b674840b56fca640279abb12efae93bded7b18e
Progress <ezeamakachisom2002@gmail.com>
2023-12-04 11:30:38 +0100

    Update ResumeBtn.js

--- src/components/ResumeBtn.js
+++ src/components/ResumeBtn.js
@@ -7,7 +7,7 @@ const ResumeBtn = ({ text }) => (
     className="bg-animate text-white text-base text-center bg-opacity-60
       duration-300 rounded bg-gray w-fit   hover:bg-animate-hover transition-all font-medium">
     <Link
-      to="https://media.publit.io/file/Progress-Ezeamaka-Resume.html"
+      to="https://media.publit.io/file/Progress-Ezeamaka.pdf"
       target="blank"
       className="flex gap-2 md:gap-3 text-center text-text hover:text-white py-2 px-4">
       {" "}
